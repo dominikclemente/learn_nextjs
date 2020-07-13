f810268339821cf346ab4dd61bdb1f8241cb7b01
Dominik Clemenete <dominik.clemente@gmail.com>
2020-07-13 21:09:04 +0100

    init(repo): starter projects - official nextjs tutorial and macek template project added

---
+++ my-next-app/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/index.css'
+
+function MyApp({ Component, pageProps }) {
+  // noinspection JSXNamespaceValidation
+    return <Component {...pageProps} />
+}
+
+export default MyApp
